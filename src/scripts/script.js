27a13dc0425ca22e2ce0a90b182d3c843aa2c1f6
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-16 03:50:47 -0300

    feat: adicionado tema para listas

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,22 @@
 const addTask = document.getElementById("button");
 const inputText = document.getElementById("task");
+const list = document.getElementById("tasks");
 const changeTheme = document.getElementById("theme");
 const body = document.getElementById("body");
-const title = document.getElementById("title");
 
 changeTheme.addEventListener("click", () => {
   body.classList.toggle("dark");
+});
+
+addTask.addEventListener("click", () => {
+  const text = inputText.value;
+  const novoItem = document.createElement("li");
+
+  if (text === "") {
+    alert("Digite uma tarefa!");
+    return;
+  }
+
+  novoItem.textContent = text;
+  list.appendChild(novoItem);
 });
