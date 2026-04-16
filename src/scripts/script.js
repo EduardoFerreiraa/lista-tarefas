ce0d07816a7592db267927131377691599bc7b17
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-16 02:44:56 -0300

    feat: adicionado EventListener mouseleave e mouseenter

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,18 @@
-const botao = document.getElementById("botao");
+const addTask = document.getElementById("button");
+const inputText = document.getElementById("task");
+const changeTheme = document.getElementById("theme");
+
+addTask.addEventListener("mouseenter", () => {
+  addTask.style.transform = "scale(1.15)";
+  addTask.style.transition = "0.5s";
+  addTask.style.background = "black";
+  addTask.style.color = "white";
+});
+
+addTask.addEventListener("mouseleave", () => {
+  addTask.style.transform = "scale(1)";
+  addTask.style.transition = "0";
+  addTask.style.background = "white";
+  addTask.style.color = "black";
+});
+
