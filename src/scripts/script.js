c9db7c9d0a0b20aeffc1a97f3a6be6474cfef2f4
Eduardo <eduardoferreiraalmeida20@gmail.com>
2026-04-16 03:16:18 -0300

    feat: adicionado sistema de tema com javascript

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,9 @@
 const addTask = document.getElementById("button");
 const inputText = document.getElementById("task");
 const changeTheme = document.getElementById("theme");
+const body = document.getElementById("body");
+const title = document.getElementById("title");
 
-addTask.addEventListener("mouseenter", () => {
-  addTask.style.transform = "scale(1.15)";
-  addTask.style.transition = "0.5s";
-  addTask.style.background = "black";
-  addTask.style.color = "white";
+changeTheme.addEventListener("click", () => {
+  body.classList.toggle("dark");
 });
-
-addTask.addEventListener("mouseleave", () => {
-  addTask.style.transform = "scale(1)";
-  addTask.style.transition = "0";
-  addTask.style.background = "white";
-  addTask.style.color = "black";
-});
-
